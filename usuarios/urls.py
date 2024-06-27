3c10059ddd90e065c30ea189369d2631e281fd3a
CRCxl <delahozcristobal8@gmail.com>
2024-06-26 22:43:32 -0400

    coimt

---
+++ usuarios/urls.py
@@ -0,0 +1,15 @@
+#from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contacto/', views.contacto , name='contacto',),
+    path('contra_olvido/', views.contra_olvido , name='contra_olvido'),
+    path('inicio_sesion/', views.inicio_sesion , name='inicio_sesion'),
+    path('registro/', views.registro , name='registro'),
+    path('trabajo1/', views.trabajo1 , name='trabajo1'),
+    path('trabajo2/', views.trabajo2 , name='trabajo2'),
+    path('trabajo3/', views.trabajo3 , name='trabajo3'),
+    path('trabajos/', views.trabajos , name='trabajos'),
+]
---
+++ usuarios/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    context={}
+    return render(request, 'usuarios/index.html', context)
+
+def contacto(request):
+    context={}
+    return render(request, 'usuarios/contacto.html', context)
+
+def contra_olvido(request):
+    context={}
+    return render(request, 'usuarios/contra_olvido.html', context)
+
+def inicio_sesion(request):
+    context={}
+    return render(request, 'usuarios/inicio_sesion.html', context)
+
+def registro(request):
+    context={}
+    return render(request, 'usuarios/registro.html', context)
+
+def trabajo1(request):
+    context={}
+    return render(request, 'usuarios/trabajo1.html', context)
+
+def trabajo2(request):
+    context={}
+    return render(request, 'usuarios/trabajo2.html', context)
+
+def trabajo3(request):
+    context={}
+    return render(request, 'usuarios/trabajo3.html', context)
+
+def trabajos(request):
+    context={}
+    return render(request, 'usuarios/trabajos.html', context)
