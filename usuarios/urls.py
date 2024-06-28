3824e10dead75076ac87ed6a115a133587befcde
MkOMIN <minibastivega93@gmail.com>
2024-06-27 20:43:06 -0400

    commit

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('contacto/', views.contacto , name='contacto',),
+    path('contra_olvido/', views.contra_olvido , name='contra_olvido'),
+    path('inicio_sesion/', views.inicio_sesion , name='inicio_sesion'),
+    path('registro/', views.registro , name='registro'),
+    path('trabajo1/', views.trabajo1 , name='trabajo1'),
+    path('trabajo2/', views.trabajo2 , name='trabajo2'),
+    path('trabajo3/', views.trabajo3 , name='trabajo3'),
+    path('trabajos/', views.trabajos , name='trabajos'),
 ]
